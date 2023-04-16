221f47394d4c68ea31d5b4f90f3c8302f3e3daff
Adolphus Miracle <miraboy13@gmail.com>
2023-04-16 21:44:12 +0100

    added GoogleTranslate Package and created LangController

--- config/app.php
+++ config/app.php
@@ -212,6 +212,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'GoogleTranslate' => Stichoza\GoogleTranslate\GoogleTranslate::class,
     ])->toArray(),
 
 ];

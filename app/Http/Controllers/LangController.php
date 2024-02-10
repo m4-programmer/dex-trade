ab002e61f4e8c063faad6de92bbc4724a2808db5
Adolphus Miracle <miraboy13@gmail.com>
2024-02-10 21:31:57 +0100

    feat: added logic to keep track of old language

--- app/Http/Controllers/LangController.php
+++ app/Http/Controllers/LangController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 class LangController extends Controller
 {
 
     public function change(Request $request)
     {
+        session()->put("old_locale", app()->getLocale());
+
         App::setLocale($request->lang);
         session()->put('locale', $request->lang);
 
